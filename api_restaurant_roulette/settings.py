729ddd46a411c2ff1afab9a7c823f8ea3bcbb549
Bijan Tabatabai <bijan311@yahoo.com>
2020-03-06 15:01:57 -0600

    Created the Restaurant model

--- api_restaurant_roulette/settings.py
+++ api_restaurant_roulette/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_restaurant_roulette.api_restaurant_roulette',
 ]
 
 MIDDLEWARE = [
