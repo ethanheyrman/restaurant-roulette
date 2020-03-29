998797fd53ee2e8b4665ef351af1eb05f5c23531
Josh <jkuzminski@wisc.edu>
2020-03-29 18:13:36 -0500

    Updated results page to match random page

--- frontend/src/Components/Results.js
+++ frontend/src/Components/Results.js
@@ -62,16 +62,42 @@ class Results extends React.Component {
             <div class="FilteredRestaurant">
                 <div class="Name">{this.state.name}</div>
                 <div class="Info">
-                    <div>{this.state.cuisine}</div>
-                    <div>{this.state.review}</div>
-                    <div>{this.state.price}</div>
-                    <div>{this.state.location}</div>
-                    <div>{this.state.number}</div>
-                    <div><a id="url" href={"https://www." + this.state.url}>{this.state.url}</a></div>
+                    <div class="Title">cuisine</div>
+                    <div class="Value">{this.state.cuisine}</div>
+                    <div class="Title">rating</div>
+                    <div class="Value">{this.getRating(this.state.review)}</div>
+                    <div class="Title">price</div>
+                    <div class="Value">{this.getPrice(this.state.price)}</div>
+                </div>
+                <div class="Contact">
+                    <div class="Title">address</div>
+                    <div class="Value">{this.state.location}</div>
+                    <div class="Title">phone number</div>
+                    <div class="Value">{this.state.number}</div>
+                    <div class="Title">website</div>
+                    <div class="Value"><a id="url" href={this.state.url}>{this.state.url}</a></div>
                 </div>
                 <div class="Hours">
-                    <div>hours</div>
-                    <div>{this.state.hours}</div>
+                    <h4>time</h4>
+                    <div>
+                        <p>{this.state.sunday_open} - {this.state.sunday_close}</p>
+                        <p>{this.state.monday_open} - {this.state.monday_close}</p>
+                        <p>{this.state.tuesday_open} - {this.state.tuesday_close}</p>
+                        <p>{this.state.wednesday_open} - {this.state.wednesday_close}</p>
+                        <p>{this.state.thursday_open} - {this.state.thursday_close}</p>
+                        <p>{this.state.friday_open} - {this.state.friday_close}</p>
+                        <p>{this.state.saturday_open} - {this.state.saturday_close}</p>
+                    </div>
+                </div>
+                <div class="Days">
+                    <h4>day</h4>
+                    <p>sunday:</p>
+                    <p>monday:</p>
+                    <p>tuesday:</p>
+                    <p>wednesday:</p>
+                    <p>thursday:</p>
+                    <p>friday:</p>
+                    <p>saturday:</p>
                 </div>
             </div>
             <div class="Navigation">
